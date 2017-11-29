0f4d274ed80b6c782348ebea66956014fbb94126
alexanderDobytin <sibsubtech@gmail.com>
2017-11-29 20:42:23 +0300

    #3

--- local/front/public/js/site.js
+++ local/front/public/js/site.js
@@ -1,7 +1,14 @@
 class Calendar {
     constructor(){
         this.itemHeight();
-        this.getData().then((data)=>this.insertData(data),(error)=>this.getItems(error));
+        
+        $('.b-calendar-header__button__next').on('click',()=>{
+            this.nextMonth();
+        })
+        $('.b-calendar-header__button__prev').on('click',()=>{
+            this.prevMonth();
+        })
+        this.start();
     }
     itemHeight(){
         $('.b-calendar-item').css({'height':$('.b-calendar-item').width()})
@@ -14,61 +21,96 @@ class Calendar {
         })
         return promise;
     }
+    start(){
+        this.getData().then((data)=>this.insertData(data),(error)=>this.getItems(error));
+    }
     insertData(data){
         console.log(data);
     }
+    setMenuDate(month,year){
+    $('.i-calendar__monthName').text(this.translateMonth(month)+' '+year);
+    }
+    nextMonth(){
+        
+        this.today.setMonth(this.today.getMonth()+1);
+        this.today == this.absoluteToday? this.today.setDate(this.absoluteToday.getDate()):this.today.setDate(1)
+        this.start()
+    }
+    prevMonth(){
+        
+        this.today.setMonth(this.today.getMonth()-1);
+        this.today == this.absoluteToday? this.today.setDate(this.absoluteToday.getDate()):this.today.setDate(1)
+        
+        this.start()
+    }
     getItems(error){
-        let callendar = "";
-        let today = new Date();
+        this.renderCounter=1;
+        this.calendarString="";
+        this.absoluteToday  = new Date();
+        let today;
+        if(this.today == undefined){today = new Date(); this.today = today} else {today = this.today;}
+      
         let day = today.getDate();
         let month = today.getMonth();
         let dayNumber = today.getDay()
         let year = today.getFullYear();
         let weekday = this.translateDay(dayNumber);
-        let firstDayCounter=0
+        this.setMenuDate(month,year);
+        let firstDayCounter=1
         let firstDayDate =new Date(year,month,1);
-        
         let firstDay = firstDayDate.getDay()
-        console.log(this.translateDay(firstDay))
+        firstDay = firstDay-1
         let lastMonth = firstDayDate;
         lastMonth.setDate(0);
-        let lastday = lastMonth.getDate();
         
+        let lastday = lastMonth.getDate();
+    
         while(firstDay>=firstDayCounter){
-           
-           
-
            let currentDay = lastday+firstDayCounter-firstDay;
-           lastMonth.setDate(currentDay)
-            console.log(lastMonth)
-            
-           // let lastWeekday = this.translateDay(lastday;)
-            
-            
-            
-            callendar+= this.itemTemplate(this.translateDay(lastMonth.getDay()),currentDay,'','');
+           lastMonth.setDate(currentDay)    
+           this.calendarString+= this.itemTemplate(this.translateDay(lastMonth.getDay()),lastMonth.getDate(),'','','');
             firstDayCounter++;
         }
+        let allCellsCounter = 35;
+        while(allCellsCounter >=  firstDayCounter){
+            let isCurrent='';
+            lastMonth.setDate(lastMonth.getDate()+1);
+            
+              lastMonth.getDate() == this.absoluteToday.getDate() && lastMonth.getMonth() == this.absoluteToday.getMonth() && lastMonth.getFullYear()==this.absoluteToday.getFullYear() ?  isCurrent = true:  isCurrent = false
+       
+            this.calendarString+= this.itemTemplate(this.translateDay(lastMonth.getDay()),lastMonth.getDate(),'','',isCurrent);
+            firstDayCounter++
+        }
         
-        $('.b-calendar__list').html(callendar)
-        this.itemHeight();
+         this.renderCallendar();
     }
-    itemTemplate(weekday,day,title,subtitle){
-            weekday ==undefined ? weekday = '':''
-            day ==undefined ? day = '':''
-            title ==undefined ? title = '':''
-            subtitle ==undefined ? subtitle = '':''
+
+    renderCallendar(){ 
+        $('.b-calendar__list').html( this.calendarString)
+        this.itemHeight();
+       
+    }
+    itemTemplate(weekday,day,title,subtitle,isCurrent){
+        
+            weekday ==undefined || this.renderCounter > 7 ? weekday = '':'';
+            day ==undefined ? day = '':'';
+            title ==undefined ? title = '':'';
+            subtitle ==undefined ? subtitle = '':'';
+ 
+            isCurrent ==true ? isCurrent = 'b-calendar-item--active' : isCurrent = '';
             let item = 
-            `<div class="b-calendar-item">
+            `<div class="b-calendar-item `+isCurrent+`">
             <div class="b-calendar-item__header">
-                <div class="b-calendar-item__weekday">`+weekday+`,</div>
+                <div class="b-calendar-item__weekday">`+weekday+`</div>
                  <div class="b-calendar__day">`+day+`</div> 
                  </div>
                 <div class="b-calendar-item__title">`+title+`</div> 
                 <div class="b-calendar-item__subtitle">`+subtitle+`</div>
             
           </div>
-            `
+            `;
+            this.renderCounter++
+            
         return item
     }
     translateDay(day){
@@ -88,6 +130,37 @@ class Calendar {
             break;
             case 6 : return 'Суббота';
             break;
+        }
+
+    }
+    translateMonth(day){
+        
+        switch (day){
+            case 0 : return 'Январь';
+            break;
+            case 1 : return 'Февраль';
+            break;
+            case 2 : return 'Март';
+            break;
+            case 3 : return 'Апрель';
+            break;
+            case 4 : return 'Май';
+            break;
+            case 5 : return 'Июнь';
+            break;
+            case 6 : return 'Июль';
+            break;
+             case 7 : return 'Август';
+            break;
+            case 8 : return 'Сентябрь';
+            break;
+            case 9 : return 'Октябрь';
+            break;
+            case 10 : return 'Ноябрь';
+            break;
+            case 11 : return 'Декабрь';
+            break;
+            
         }
 
     }
